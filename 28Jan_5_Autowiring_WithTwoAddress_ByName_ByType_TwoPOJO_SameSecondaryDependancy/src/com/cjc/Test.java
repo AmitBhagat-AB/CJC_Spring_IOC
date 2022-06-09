402b3781a4b7cfa6f98db8732842fc62eb7ec051
AmitBhagat-AB <92724811+AmitBhagat-AB@users.noreply.github.com>
2022-06-09 21:12:18 +0530

    28Jan_5_Autowiring_WithTwoAddress_ByName_ByType
    _TwoPOJO_SameSecondaryDependancy

---
+++ 28Jan_5_Autowiring_WithTwoAddress_ByName_ByType_TwoPOJO_SameSecondaryDependancy/src/com/cjc/Test.java
@@ -0,0 +1,25 @@
+package com.cjc;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+	public static void main(String[] args) 
+	{
+		ApplicationContext aps=new ClassPathXmlApplicationContext("APC.xml");
+		
+		Student stu=aps.getBean("s",Student.class);
+		System.out.println(stu.getRollno());
+		System.out.println(stu.getName());
+		System.out.println(stu.getAddr().getAreaName());
+		System.out.println(stu.getAddr().getCityName());
+		
+		
+		Employee emp=aps.getBean("e",Employee.class);
+		System.out.println(emp.getEid());
+		System.out.println(emp.getEname());
+		System.out.println(emp.getEaddr().getAreaName());
+		System.out.println(emp.getEaddr().getCityName());
+	}
+
+}
